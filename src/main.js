4af8d497e2b0f737cd32802db7ec8e9113a84459
LLLxj <253358001@qq.com>
2019-03-29 15:48:40 +0800

    更新全局main配置

--- src/main.js
+++ src/main.js
@@ -5,9 +5,7 @@ import 'normalize.css/normalize.css'// A modern alternative to CSS resets
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import locale from 'element-ui/lib/locale/lang/zh-CN' // lang i18n
-
 import '@/styles/index.scss' // global css
-
 import App from './App'
 import router from './router'
 import store from './store'
@@ -21,7 +19,7 @@ import 'quill/dist/quill.core.css'
 import 'quill/dist/quill.snow.css'
 import 'quill/dist/quill.bubble.css'
 
-Vue.use(ElementUI, { locale })
+Vue.use(ElementUI, {size: 'small'})
 Vue.use(xlog)
 Vue.use(VueQuillEditor)
 
