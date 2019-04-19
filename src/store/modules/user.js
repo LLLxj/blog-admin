3a4bbb9b3977840657188fa15627030521484523
LLLxj <253358001@qq.com>
2019-04-19 14:39:28 +0800

    更新store模块

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -30,6 +30,7 @@ const user = {
       const username = userInfo.username.trim()
       return new Promise((resolve, reject) => {
         login(username, userInfo.password).then(response => {
+          console.log(response)
           const data = response.data
           setToken(data.token)
           commit('SET_TOKEN', data.token)
