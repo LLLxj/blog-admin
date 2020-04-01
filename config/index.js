8dec7b07969237e1794adcbc48708606608f698e
LLLxj <253358001@qq.com>
2020-04-01 10:19:13 +0800

    更新配置

--- config/index.js
+++ config/index.js
@@ -36,7 +36,6 @@ module.exports = {
     },
 
     // Various Dev Server settings
-    // host: 'http://112.74.162.61',
     // host: 'localhost', // can be overwritten by process.env.HOST
     host: '0.0.0.0', // can be overwritten by process.env.HOST
     // host: 'http://112.74.162.61', // can be overwritten by process.env.HOST
